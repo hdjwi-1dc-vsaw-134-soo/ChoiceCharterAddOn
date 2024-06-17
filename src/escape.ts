7c5e8ad355bd5a510603b052f4a4b009a46f1be3
mamasitta <vikiber2013@gmail.com>
2024-06-17 09:07:46 +0200

    test1

--- src/escape.ts
+++ src/escape.ts
@@ -1,6 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 //import { getQuest } from "@workadventure/quests";
-import { levelUp } from "@workadventure/quests";
+//import { levelUp } from "@workadventure/quests";
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
